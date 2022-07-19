f4b7d4f632fd77f8267b2ab5265751ef2c5c2ddb
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-19 17:55:32 -0300

    update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
 //const convidado = document.getElementById9('convidado');
-
 const convidado = prompt('Digite seu nome: ');
-document.getElementById('convidado').innerHTML = convidado;
+
+convidado.length >= 3 ?
+document.getElementById('convidado').innerHTML = convidado.charAt(0).toUpperCase() + convidado.slice(1, 50)
+: alert('Seu nome precisa ter mais de 3 ou mais caracteres') ? false : location.reload();
